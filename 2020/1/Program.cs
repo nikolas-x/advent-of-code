f25f3b1064a9ff42fa39959ca774dbdd2ee0ed31
Nikolas Xexenis (Home) <nikolasx@gmail.com>
2021-09-27 01:32:02 +1000

    Reading data

--- 2020/1/Program.cs
+++ 2020/1/Program.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        var directory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        var lines = File.ReadAllLines($"{directory}/data/input");
+        var entries = Array.ConvertAll(lines, int.Parse);
+        Array.ForEach(entries, Console.WriteLine);
     }
 }
